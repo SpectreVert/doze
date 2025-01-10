8f3d87c6d17efa6cdc85e155896ee30813c1a7f4
Costa Bushnaq <bushnaq.costa@gmail.com>
2025-01-10 15:31:06 +0100

    rebuilder: fix extra Rules being planned when reaching
    already scheduled Rules. We are using a flag in the DeepRebuilder
    struct itself.

--- rebuilder.go
+++ rebuilder.go
@@ -7,10 +7,13 @@ import (
 // A Rebuilder is given a list of Artifacts to bring up to date and computes a list of Rules to execute
 // or inspect to achieve the wanted goal.
 type Rebuilder interface {
+	cleanup(*Graph) // should be called before each rebuild
 	rebuild([]ArtifactTag, *Graph) ([]Rule, error)
 }
 
-type DeepRebuilder struct{}
+type DeepRebuilder struct {
+	nothingNewFound bool
+}
 
 func (dr *DeepRebuilder) rebuild(targets []ArtifactTag, level int, g *Graph) ([]*Rule, error) {
 	if g == nil {
@@ -19,6 +22,7 @@ func (dr *DeepRebuilder) rebuild(targets []ArtifactTag, level int, g *Graph) ([]
 	if targets == nil {
 		return nil, nil
 	}
+	dr.nothingNewFound = true
 	var plan []*Rule
 	for _, target := range targets {
 		a, ok := g.artifacts[target]
@@ -34,6 +38,7 @@ func (dr *DeepRebuilder) rebuild(targets []ArtifactTag, level int, g *Graph) ([]
 			// if all of the other artifacts are primordial or scheduled, the plan will be empty.
 			// this lets the caller know to schedule the originating creatorRule. If the plan
 			// ends up not empty, then there is a higher creatorRule and we must continue.
+			dr.nothingNewFound = false
 			continue
 		}
 
@@ -46,12 +51,17 @@ func (dr *DeepRebuilder) rebuild(targets []ArtifactTag, level int, g *Graph) ([]
 		tmpPlan, err := dr.rebuild(a.creatorRule.inputs, level+1, g)
 		if err != nil {
 			return nil, err
-		} else if tmpPlan == nil {
-			// we couldn't get higher; the creatorRule should be scheduled (except if it's because everything was already scheduled..)
+		}
+		if tmpPlan == nil {
+			// we couldn't get higher; the creatorRule should be scheduled
+			if dr.nothingNewFound {
+				// except if nothing new was found in the previous run, which means we reached only rules that were already scheduled
+				continue
+			}
 			a.creatorRule.scheduled = true
 			plan = append(plan, a.creatorRule)
 		} else {
-			// we went higher in the dependency graph, schedule the results
+			// we went higher in the dependency graph, schedule what we got back
 			plan = append(plan, tmpPlan...)
 		}
 	}
